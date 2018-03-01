ce9fc2ef57bb7eaa877a78b89cf5abc1fe0b754c
Daniel <daniel.heinesen@sf-nett.no>
2018-03-01 11:37:13 +0100

    Nearly optimized

--- Project1/project1.py
+++ Project1/project1.py
@@ -16,14 +16,19 @@ Z_Be7 = 1e-7
 
 class stellar_core:
     def __init__(self,filename="opacity.txt"):
+
+        #Reads and interpolates the opacity file
         self.file = filename
         self.read_opacity()
+
+        #Sets the constants needed for the simulation
         self.G = 6.67408e-11
         self.k = 1.3806505e-23
         self.m_u = 1.660539040e-27
         self.a = 7.5657e-16
-        self.mu = 1/(X+Y/4.+Z/2.)
+        self.mu = 1/2.*1/(X+Y/4.+Z/2.)
         self.sigma = 5.670367e-8
+
 
     def read_opacity(self):
             self.opacity = []
@@ -45,26 +50,25 @@ class stellar_core:
 
 
     def get_opacity(self,T,rho):
+        #Makes R from rho and T. Takes the log_10 of R and T
         log_R = np.log10(rho*.001/(T*1e-6)**3)
         log_T = np.log10(T)
 
+        #Gets log_10(kappa) in cgs units
         log_kappa = self.opacity_interp2d(log_R,log_T)
 
+        #Returns kappa in SI
         return 10**log_kappa*0.1
-
 
 
 
     def solve(self,step,L0,R0,M0,rho0,T0,adaptive=True):
         dm = step
 
-        M = []
-        R = []
-        L = []
-        rho = []
-        T = []
-        P = []
+        #Initiaze empty lists
+        M = []; R = []; L = []; rho = []; T = []; P = []
 
+        #Appends the initial values to the empty lists
         M.append(M0)
         R.append(R0)
         L.append(L0)
@@ -72,55 +76,81 @@ class stellar_core:
         T.append(T0)
         P.append(self.P(rho0,T0))
 
+
+        #Makes arrays for the use in the adaptive steps
         dms = np.zeros(4)
         dV = np.zeros(4)
-
         V = np.zeros(4)
 
+        #Tolerence for the change dV/V, used for adaptive steps
         p = 1e-4
 
-        while M[-1] >= 0:
-            R.append(R[-1] + dm*self.dr_dm(R[-1],rho[-1]))
-            P.append(P[-1] + dm*self.dP_dm(M[-1],R[-1]))
-            L.append(L[-1] + dm*self.dL_dm(rho[-1],T[-1]))
-            T.append(T[-1] + dm*self.dT_dm(L[-1],R[-1],T[-1],rho[-1]))
+        while M[-1] >= 0.05*M0:
+            try:
+                #Integrates using first order Euler
+                R.append(R[-1] + dm*self.dr_dm(R[-1],rho[-1]))
+                P.append(P[-1] + dm*self.dP_dm(M[-1],R[-2]))
+                L.append(L[-1] + dm*self.dL_dm(rho[-1],T[-1]))
+                T.append(T[-1] + dm*self.dT_dm(L[-2],R[-2],T[-1],rho[-1]))
+
+                rho.append(self.rho(P[-1],T[-1]))
+
+                M.append(M[-1] + dm)
 
 
+                #Handles adaptive steps
+                if adaptive:
 
+                    dV[0] = R[-2]-R[-1]
+                    dV[1] = P[-2]-P[-1]
+                    dV[2] = L[-2]-L[-1]
+                    dV[3] = T[-2]-T[-1]
 
+                    V[0] = R[-1]
+                    V[1] = P[-1]
+                    V[2] = L[-1]
+                    V[3] = T[-1]
 
-            dV[0] = R[-2]-R[-1]
-            dV[1] = P[-2]-P[-1]
-            dV[2] = L[-2]-L[-1]
-            dV[3] = T[-2]-T[-1]
+                    dV_V = np.abs(dV)/V
 
-            V[0] = R[-1]
-            V[1] = P[-1]
-            V[2] = L[-1]
-            V[3] = T[-1]
+                    if (True in (dV_V>p)):
+                        g = np.abs(dV)/np.abs(dm)
+                        dm = -np.min(p*V/g)
+                        #print("Changing dm to {}".format(dm))
 
-            dV_V = np.abs(dV)/V
+                # if abs(dm) < 1e20:
+                #     print("To small dm")
+                #     break
+                #
 
-            if True in (dV_V>p):
-                g = np.abs(dV)/np.abs(dm)
-                dm = -np.min(p*V/g)
-                print("Changing dm to {}".format(dm))
+                # if R[-1] < 0.05*R0:
+                #     break
 
+                #Checks if certain values are below zero to stop non-phyical behavior
+                if (R[-1]<=0.0*R0) or (L[-1] <= 0.0*L0):
+                    print("At mass {} either temperature, luminocity or density is zeros".format(M[-1]))
+                    print("Rho: ",rho[-1])
+                    print("T: ",T[-1])
+                    print("L: ",L[-1])
+                    break
 
-            rho.append(self.rho(P[-1],T[-1]))
-
-            M.append(M[-1] + dm)
-
-            if (rho[-1]<=0) or (T[-1]<=0):
-                print("At mass {} either temperature or density is zeros".format(M[-1]))
-                break
-
-
+            except KeyboardInterrupt:
+                leng = min([len(M),len(R),len(L),len(rho),len(T)])
+                plt.plot(np.array(M[:leng])/M0,np.array(R[:leng])/R0)
+                plt.show()
+                plt.plot(np.array(M[:leng])/M0,np.array(L[:leng])/L0)
+                plt.show()
+                plt.plot(np.array(M[:leng])/M0,np.array(T[:leng])/T0)
+                plt.show()
+                plt.plot(np.array(M[:leng])/M0,np.array(rho[:leng])/rho0)
+                plt.show()
+                exit()
         return np.array(M),np.array(R),np.array(L),np.array(rho),\
                             np.array(T),np.array(P)
 
 
 
+    #Functions for the values and derivatives of the needed quantities
     def dr_dm(self,r,rho):
         return 1./(4*np.pi*r**2*rho)
 
@@ -139,8 +169,92 @@ class stellar_core:
         return rho/(self.mu*self.m_u)*self.k*T + self.a/3.*T**4
 
     def rho(self,P,T):
-
         return (P - self.a/3.*T**4)*(self.mu*self.m_u)/(self.k*T)
+
+    #Method for optimizing initial values
+    def optimize_initial_condition(self,step,L0,R0,M0,rho0,T0):
+        # L = L0
+        # R = R0
+        # M = M0
+        # rho = rho0
+        # T = T0
+        init_values = np.array([R0,L0,rho0,T0])
+        prev_init_values = np.copy(init_values)
+
+        change = np.zeros(4)
+        prev_change = np.zeros(4)
+
+        last_values = np.zeros(4)
+        prev_last_values = np.zeros(4)
+
+
+        search = True
+        number_of_stagnations = 0
+        number_of_searches = 0
+
+        eps = 1e-6
+
+        while search:
+
+            R0 = init_values[0]
+            L0 = init_values[1]
+            rho0 = init_values[2]
+            T0 = init_values[3]
+
+            print("Search nr. {} with {}, {}, {}, {} and {}".format(number_of_searches,M0,R0,L0,rho0,T0))
+            number_of_searches += 1
+
+
+
+            M,R,L,rho,T,P = self.solve(step,L0,R0,M0,rho0,T0,adaptive=False)
+
+
+            last_values[0] = R[-1]
+            last_values[1] = L[-1]
+            last_values[2] = rho[-1]
+            last_values[3] = T[-1]
+
+            change = (last_values - prev_last_values)/(last_values)
+            r = np.random.random()
+            if np.sum(prev_last_values)/(np.sum(last_values) + eps) >= r:#np.sum(change) > np.sum(prev_change):
+                prev_init_values  = init_values
+                prev_last_values = last_values
+                prev_change = change
+
+                number_of_stagnations = 0
+            else:
+                number_of_stagnations += 1
+
+            # if number_of_stagnations >= 10:
+            #     print("Minumum reached with: ", init_values)
+            #     search = False
+
+
+            init_values += (np.random.normal(size=4)*0.05)*init_values #0.5*change*init_values
+
+
+
+        M0 = init_values[0]
+        R0 = init_values[1]
+        L0 = init_values[2]
+        rho0 = init_values[3]
+        T0 = init_values[4]
+        M,R,L,rho,T,P = self.solve(step,L0,R0,M0,rho0,T0,adaptive=False)
+        plt.plot(M/M0,R/R0)
+        plt.show()
+        plt.plot(M/M0,L/L0)
+        plt.show()
+        plt.plot(M/M0,T/T0)
+        plt.show()
+        plt.plot(M/M0,rho/rho0)
+        plt.show()
+
+
+
+
+
+
+
 
 
 
@@ -160,7 +274,34 @@ if __name__ == '__main__':
     T0 = 5.7e6
     M_step = -1e-4*M_sun
 
-    M,R,L,rho,T,P = sc.solve(M_step,L0,R0,M0,rho0,T0)
+    #M0 *=0.5
+    L0 *=.9
+    R0 *=0.5
+    rho0 *=0.5
+    T0 *=0.8
+
+    #sc.optimize_initial_condition(M_step,L0,R0,M0,rho0,T0)
+    M,R,L,rho,T,P = sc.solve(M_step,L0,R0,M0,rho0,T0,adaptive=False)
     #print(R/R0)
-    plt.semilogy(M/M0,rho/avg_rho)
+    #plt.semilogy(M/M0,rho/avg_rho)
+    print(sc.rho(sc.P(rho0,T0),T0),rho0) #Check for Rho(P(rho0,T0),T0) = rho0
+    print(M[-1]/M0)
+    print(R[-1]/R0)
+    print(L[-1]/L0)
+
+    plt.plot(M/M0,R/R0)
+    plt.title("R")
+    plt.show()
+    plt.plot(M/M0,L/L0)
+    plt.title("L")
+    plt.show()
+    exit()
+    plt.plot(M/M0,rho/rho0)
+    plt.title("Rho")
+    plt.show()
+    plt.plot(M/M0,T/T0)
+    plt.title("T")
+    plt.show()
+    plt.plot(M/M0,P/sc.P(rho0,T0))
+    plt.title("P")
     plt.show()
